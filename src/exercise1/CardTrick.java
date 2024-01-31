d6d5c7f658d08082b6bf5b25cea829c6c3393e3c
Parv Patel <157135948+Parv2817@users.noreply.github.com>
2024-01-31 09:09:27 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -60,19 +60,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Parv Patel, but you can call me Parv");
         System.out.println();
         
         System.out.println("My career ambitions:");
         System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Cloud Computing and AI-ML enthusiast");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Travelling");
+        System.out.println("-- Cycling");
+        System.out.println("-- Watching Movies");
+        System.out.println("-- Swimming");
 
         System.out.println();
         
